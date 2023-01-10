05ae858ffb66fa702f03ee3e6d656630a3a37e1b
Iris2030 <ibazhinova@gmail.com>
2023-01-10 11:28:21 +0200

    store changed

--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -1,28 +1,57 @@
 import { defineStore } from "pinia";
+import { computed, ref } from "vue";
 
-export const useMovieStore = defineStore("movieStore", {
-  state: () => ({
-    movies: [],
-    activeTab: 2,
-  }),
-  getters: {
-    watchedMovies() {
-      return this.movies.filter((movie) => movie.isWatched);
-    },
-    totalCountMovies() {
-      return this.movies.length;
-    },
-  },
-  actions: {
-    setActiveTab(id) {
-      this.activeTab = id;
-    },
-    toggleWatch(id) {
-      const index = this.movies.findIndex((movie) => movie.id === id);
-      this.movies[index].isWatched = !this.movies[index].isWatched;
-    },
-    deleteMovie(id) {
-        return this.movies = this.movies.filter(movie => movie.id !== id);
-       },
-  },
+// export const useMovieStore = defineStore("movieStore", {
+//   state: () => ({
+//     movies: [],
+//     activeTab: 2,
+//   }),
+//   getters: {
+//     watchedMovies() {
+//       return this.movies.filter((movie) => movie.isWatched);
+//     },
+//     totalCountMovies() {
+//       return this.movies.length;
+//     },
+//   },
+//   actions: {
+//     setActiveTab(id) {
+//       this.activeTab = id;
+//     },
+//     toggleWatch(id) {
+//       const index = this.movies.findIndex((movie) => movie.id === id);
+//       this.movies[index].isWatched = !this.movies[index].isWatched;
+//     },
+//     deleteMovie(id) {
+//         return this.movies = this.movies.filter(movie => movie.id !== id);
+//        },
+//   },
+// });
+
+export const useMovieStore = defineStore("movieStore", () => {
+  const movies = ref([]);
+  const activeTab = ref(2);
+
+  const watchedMovies = computed(() =>{
+          return movies.value.filter((movie) => movie.isWatched);
+  })
+
+  const totalCountMovies = computed(() =>{
+    return movies.value.length;
+})
+
+  const setActiveTab = (id) => {
+    activeTab.value = id
+  };
+
+  const toggleWatch = (id) => {
+          const index = movies.value.findIndex((movie) => movie.id === id);
+          movies.value[index].isWatched = !movies.value[index].isWatched;
+        }
+
+        const deleteMovie = (id) => {
+        return movies.value = movies.value.filter(movie => movie.id !== id);
+       }
+
+  return {activeTab, movies, toggleWatch, setActiveTab, deleteMovie,watchedMovies,totalCountMovies}
 });
--- src/stores/SearchStore.js
+++ src/stores/SearchStore.js
@@ -5,35 +5,47 @@ import { ref } from "vue";
 const url =
   "https:api.themoviedb.org/3/search/movie?api_key=967c6f14dacb0ca10f1175f7851a5869&query=";
 
-export const useSearchStore = defineStore("searchstore", {
-  state: () => ({
-movies:[],
-loader: false
-       }),
-    actions:{
-        async getMovies(search){
-            this.loader = true
-          const res =  await fetch(`${url}${search}`)
-          const data = await res.json()
-          console.log('fetching...');
-         this.movies = data.results
-         this.loader = false
+// export const useSearchStore = defineStore("searchstore", {
+//   state: () => ({
+// movies:[],
+// loader: false
+//        }),
+//     actions:{
+//         async getMovies(search){
+//             this.loader = true
+//           const res =  await fetch(`${url}${search}`)
+//           const data = await res.json()
+//           console.log('fetching...');
+//          this.movies = data.results
+//          this.loader = false
 
-        },
-        addToFavMovies(object){
-            const movieStore = useMovieStore();
-            movieStore.movies.push({...object, isWatched: false})
-            console.log(object);
-            movieStore.activeTab = 1
-        }
-    }
+//         },
+//         addToFavMovies(object){
+//             const movieStore = useMovieStore();
+//             movieStore.movies.push({...object, isWatched: false})
+//             console.log(object);
+//             movieStore.activeTab = 1
+//         }
+//     }
+// });
+
+export const useSearchStore = defineStore("searchstore", () => {
+  const loader = ref(false);
+  const movies = ref([]);
+
+  const getMovies = async (search) => {
+    loader.value = true;
+    const res = await fetch(`${url}${search}`);
+    const data = await res.json();
+    movies.value = data.results;
+    loader.value = false;
+  };
+
+  const addToFavMovies = (object) => {
+    const movieStore = useMovieStore();
+    movieStore.movies.push({ ...object, isWatched: false });
+    movieStore.activeTab = 1;
+  };
+
+  return {loader, movies, getMovies, addToFavMovies}
 });
-
-// export const useSearchStore = defineStore("searchstore", () =>{
-// const loader = ref(false)
-// const movies = ref([])
-
-// const getMovies = async () => {
-    
-// }
-// })
